b88b7336dc12ec0dd421f1caade81e6760afe409
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-04-10 11:24:40 +0900

    initialize

---
+++ ProvisioningProfileExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProvisioningProfileExplorer
+//
+//  Created by hirauchi.shinichi on 2016/04/10.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProvisioningProfileExplorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProvisioningProfileExplorer
+//
+//  Created by hirauchi.shinichi on 2016/04/10.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
