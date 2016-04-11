e3b454f757a9d4d34cb6bf92a8b96fd36bbe94d9
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-04-11 10:08:37 +0900

    append profile

--- ProvisioningProfileExplorer/ProvisioningProfile.swift
+++ ProvisioningProfileExplorer/ProvisioningProfile.swift
@@ -21,7 +21,9 @@ class ProvisioningProfile: NSObject {
 
     var uuid: NSString? = nil
     var teamName: NSString? = nil
-
+    //var teamIdentifier: NSArray? = nil
+    var teamIdentifier = [String]();
+    var provisionedDevices = [String]();
 
 
     private var _expirationDate: NSDate? = nil
@@ -82,6 +84,27 @@ class ProvisioningProfile: NSObject {
         // TeamName
         teamName = plist.objectForKey("TeamName") as! NSString
 
+        // TeamIdentifier
+        teamIdentifier = (plist.objectForKey("TeamIdentifier") as? [String])!
+
+        // ProvisionedDevices
+        if let devices = plist.objectForKey("ProvisionedDevices") as? [String] {
+            //NSArray* cNameSorted = [cName sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+            provisionedDevices = devices.sort{ $0 < $1 }
+        }
+
+        var x=0;
+
+//        value = [propertyList objectForKey:@"ProvisionedDevices"];
+//        if ([value isKindOfClass:[NSArray class]]) {
+//            //[synthesizedInfo addEntriesFromDictionary:formattedDevicesData(value)];
+//        } else {
+//            [synthesizedInfo setObject:@"No Devices" forKey:@"ProvisionedDevicesFormatted"];
+//            [synthesizedInfo setObject:@"Distribution Profile" forKey:@"ProvisionedDevicesCount"];
+//        }
+
+
+
 //        id creationDate = [propertyList objectForKey:@"CreationDate"];
 //        if ([creationDate isKindOfClass:[NSDate class]]) {
 //            NSDate *date = (NSDate *)value;
@@ -101,6 +124,8 @@ class ProvisioningProfile: NSObject {
 
 
     }
+
+
 
     // 暗号データの復号
     func decode(encryptedData:NSData) -> NSData? {
@@ -139,16 +164,45 @@ class ProvisioningProfile: NSObject {
         html.appendString("<html>")
         html.appendString("<style>")
 //        html.appendString(".name{ color:ff0000; background-color:#FFCACA }")
-        html.appendString("body{ color:000000; font-size:12px; font-family: \"Hiragino Kaku Gothic ProN\", sans-serif;}")
+        html.appendString("body{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
+        html.appendString("table{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
 //        html.appendString("ul{ list-style-type: none;margin: 0 0 0 1.5em;}")
-        html.appendString(".name{ color:006600; font-size:16px;margin:20,0;font-weight: bold;}")
+        html.appendString(".name{ margin:10px 0px 5px 0px;color:006600; font-size:16px;margin:20,0;font-weight: bold;}")
+        html.appendString(".title{ margin:10px 0px 5px 0px;font-weight: bold;}")
         html.appendString("</style>")
 
-        html.appendString(String(format: "<br><span class=\"name\">%@</span>",_name!))
+        html.appendString(String(format: "<div class=\"name\">%@</div>",_name!))
         html.appendString(String(format: "<br>Profile UUID: %@",uuid!))
-        html.appendString(String(format: "<br>Profile Team: %@",teamName!))
+        html.appendString(String(format: "<br>Profile Team: %@ ",teamName!))
+        if teamIdentifier.count>0 {
+            html.appendString("(")
+            for var i in teamIdentifier {
+                html.appendString(String(format: "%@ ",i as! NSString))
+            }
+            html.appendString(")")
+        }
         html.appendString(String(format: "<br>Creation Date: %@",LocalDate(_createDate!)))
         html.appendString(String(format: "<br>Expiretion Date: %@",LocalDate(_expirationDate!)))
+
+        // DEVICES
+        html.appendString("<div class=\"title\">DEVICES ")
+        html.appendString(String(format: "(%d DEVICES)",provisionedDevices.count))
+        html.appendString("</div>")
+        html.appendString("<table>")
+        html.appendString("<tr>")
+        html.appendString("<td></td>")
+        html.appendString("<td>UUID</td>")
+        html.appendString("</tr>")
+        for var d in provisionedDevices {
+            html.appendString("<tr>")
+            html.appendString(String(format: "<td>*</td>"))
+            html.appendString(String(format: "<td>%@</td>",d as! NSString))
+            html.appendString("</tr>")
+        }
+        html.appendString("</table>")
+
+
+
         html.appendString("</html>")
 
         return html as String
