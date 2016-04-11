eca6e0aac100680b74b121629c3a988db5064474
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-04-12 08:55:51 +0900

    touch

--- ProvisioningProfileExplorer/ProvisioningProfile.swift
+++ ProvisioningProfileExplorer/ProvisioningProfile.swift
@@ -12,34 +12,13 @@ class ProvisioningProfile: NSObject {
 
     private var _path = ""
 
-    private var _name: NSString? = nil
-    var name:NSString {
-        get{
-            return _name!
-        }
-    }
-
-    var uuid: NSString? = nil
-    var teamName: NSString? = nil
-    //var teamIdentifier: NSArray? = nil
-    var teamIdentifier = [String]();
+    var name: String = ""
+    var uuid: String = ""
+    var teamName: String = ""
+    var teamIdentifier = [String]()
     var provisionedDevices = [String]();
-
-
-    private var _expirationDate: NSDate? = nil
-    var expirationDate:NSString{
-        get{
-            return LocalDate(_expirationDate!)
-        }
-    }
-
-    private var _createDate: NSDate? = nil
-    var createDate:NSString{
-        get{
-            return LocalDate(_createDate!)
-        }
-    }
-
+    var expirationDate: NSDate = NSDate(timeIntervalSinceReferenceDate: 0)
+    var creationDate: NSDate = NSDate(timeIntervalSinceReferenceDate: 0)
 
     init(path:String){
         super.init()
@@ -51,15 +30,12 @@ class ProvisioningProfile: NSObject {
 
     func Interpretation(){
 
-        // ファイルの読み込み
         guard let encryptedData = NSData(contentsOfFile: _path) else {
             return
         }
-        // データの復号
         guard let plistData = decode(encryptedData) else{
             return
         }
-        //プロパティリストへの変換
         guard let plist = try? NSPropertyListSerialization.propertyListWithData(plistData, options: NSPropertyListMutabilityOptions.MutableContainersAndLeaves, format: nil) as! NSDictionary else {
             return
         }
@@ -68,32 +44,16 @@ class ProvisioningProfile: NSObject {
         for key in plist.allKeys {
             //print("key:\(key) value:\(plist.objectForKey(key))")
         }
-
-        // 名前
-        _name = plist.objectForKey("Name") as! NSString
-
-        // 作成年月日
-        _createDate = plist.objectForKey("CreationDate") as! NSDate
-
-        // 有効期限
-        _expirationDate = plist.objectForKey("ExpirationDate") as! NSDate
-
-        // UUID
-        uuid = plist.objectForKey("UUID") as! NSString
-
-        // TeamName
-        teamName = plist.objectForKey("TeamName") as! NSString
-
-        // TeamIdentifier
+        name = plist.objectForKey("Name") as! String
+        creationDate = plist.objectForKey("CreationDate") as! NSDate
+        expirationDate = plist.objectForKey("ExpirationDate") as! NSDate
+        uuid = plist.objectForKey("UUID") as! String
+        teamName = plist.objectForKey("TeamName") as! String
         teamIdentifier = (plist.objectForKey("TeamIdentifier") as? [String])!
-
-        // ProvisionedDevices
         if let devices = plist.objectForKey("ProvisionedDevices") as? [String] {
-            //NSArray* cNameSorted = [cName sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
             provisionedDevices = devices.sort{ $0 < $1 }
         }
 
-        var x=0;
 
 //        value = [propertyList objectForKey:@"ProvisionedDevices"];
 //        if ([value isKindOfClass:[NSArray class]]) {
@@ -126,6 +86,21 @@ class ProvisioningProfile: NSObject {
     }
 
 
+    // ローカルタイムでのNSDate表示
+    func LocalDate(date: NSDate) -> NSString {
+        let calendar = NSCalendar.currentCalendar()
+        let comps = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate:date)
+        let year = comps.year
+        let month = comps.month
+        let day = comps.day
+        let hour = comps.hour
+        let minute = comps.minute
+        _ = comps.second
+
+        return NSString(format: "%04d/%02d/%02d %02d:%02d", year,month,day,hour,minute)
+        //        return "\(year)/\(month)/\(day) \(hour):\(minute)"
+    }
+
 
     // 暗号データの復号
     func decode(encryptedData:NSData) -> NSData? {
@@ -139,72 +114,52 @@ class ProvisioningProfile: NSObject {
         return decodedData
     }
 
-    // ローカルタイムでのNSDate表示
-    func LocalDate(date: NSDate) -> NSString {
-        let calendar = NSCalendar.currentCalendar()
-
-        //let calendar = NSCalendar(identifier: NSGregorianCalendar)
-        //let calendar = NSCalendar(identifier: NSJapaneseCalendar)
-
-        let comps = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate:date)
-        let year = comps.year
-        let month = comps.month
-        let day = comps.day
-        let hour = comps.hour
-        let minute = comps.minute
-        _ = comps.second
-
-        return NSString(format: "%04d/%02d/%02d %02d:%02d", year,month,day,hour,minute)
-//        return "\(year)/\(month)/\(day) \(hour):\(minute)"
-    }
-
     func generateHTML() -> String {
-        let html = NSMutableString()
+        var html = "<html>"
+        html.appendContentsOf("<html>")
 
-        html.appendString("<html>")
-        html.appendString("<style>")
-//        html.appendString(".name{ color:ff0000; background-color:#FFCACA }")
-        html.appendString("body{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
-        html.appendString("table{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
-//        html.appendString("ul{ list-style-type: none;margin: 0 0 0 1.5em;}")
-        html.appendString(".name{ margin:10px 0px 5px 0px;color:006600; font-size:16px;margin:20,0;font-weight: bold;}")
-        html.appendString(".title{ margin:10px 0px 5px 0px;font-weight: bold;}")
-        html.appendString("</style>")
+        html.appendContentsOf("<html>")
+        html.appendContentsOf("<style>")
+        html.appendContentsOf("body{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
+        html.appendContentsOf("table{ color:000000; font-size:12px; font-family: \"Menlo\", sans-serif;}")
+        html.appendContentsOf(".name{ margin:10px 0px 5px 0px;color:006600; font-size:16px;margin:20,0;font-weight: bold;}")
+        html.appendContentsOf(".title{ margin:10px 0px 5px 0px;font-weight: bold;}")
+        html.appendContentsOf("</style>")
 
-        html.appendString(String(format: "<div class=\"name\">%@</div>",_name!))
-        html.appendString(String(format: "<br>Profile UUID: %@",uuid!))
-        html.appendString(String(format: "<br>Profile Team: %@ ",teamName!))
+        html.appendContentsOf(String(format: "<div class=\"name\">%@</div>",name))
+        html.appendContentsOf(String(format: "<br>Profile UUID: %@",uuid))
+        html.appendContentsOf(String(format: "<br>Profile Team: %@ ",teamName))
         if teamIdentifier.count>0 {
-            html.appendString("(")
-            for var i in teamIdentifier {
-                html.appendString(String(format: "%@ ",i as! NSString))
+            html.appendContentsOf("(")
+            for var team in teamIdentifier {
+                html.appendContentsOf(String(format: "%@ ",team))
             }
-            html.appendString(")")
+            html.appendContentsOf(")")
         }
-        html.appendString(String(format: "<br>Creation Date: %@",LocalDate(_createDate!)))
-        html.appendString(String(format: "<br>Expiretion Date: %@",LocalDate(_expirationDate!)))
+        html.appendContentsOf(String(format: "<br>Creation Date: %@",LocalDate(creationDate)))
+        html.appendContentsOf(String(format: "<br>Expiretion Date: %@",LocalDate(expirationDate)))
 
         // DEVICES
-        html.appendString("<div class=\"title\">DEVICES ")
-        html.appendString(String(format: "(%d DEVICES)",provisionedDevices.count))
-        html.appendString("</div>")
-        html.appendString("<table>")
-        html.appendString("<tr>")
-        html.appendString("<td></td>")
-        html.appendString("<td>UUID</td>")
-        html.appendString("</tr>")
-        for var d in provisionedDevices {
-            html.appendString("<tr>")
-            html.appendString(String(format: "<td>*</td>"))
-            html.appendString(String(format: "<td>%@</td>",d as! NSString))
-            html.appendString("</tr>")
+        html.appendContentsOf("<div class=\"title\">DEVICES ")
+        html.appendContentsOf(String(format: "(%d DEVICES)",provisionedDevices.count))
+        html.appendContentsOf("</div>")
+        html.appendContentsOf("<table>")
+        html.appendContentsOf("<tr>")
+        html.appendContentsOf("<td></td>")
+        html.appendContentsOf("<td>UUID</td>")
+        html.appendContentsOf("</tr>")
+        for var device in provisionedDevices {
+            html.appendContentsOf("<tr>")
+            html.appendContentsOf(String(format: "<td>*</td>"))
+            html.appendContentsOf(String(format: "<td>%@</td>",device))
+            html.appendContentsOf("</tr>")
         }
-        html.appendString("</table>")
+        html.appendContentsOf("</table>")
 
 
 
-        html.appendString("</html>")
+        html.appendContentsOf("</html>")
 
-        return html as String
+        return html
     }
 }
--- ProvisioningProfileExplorer/ViewController.swift
+++ ProvisioningProfileExplorer/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     @IBOutlet weak var webView: WebView!
 
 
-    var profiles : [ProvisioningProfile] = []
+    var profiles :[ProvisioningProfile] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         let path =  NSHomeDirectory() + "/Library/MobileDevice/Provisioning Profiles"
         if let files = try? manager.contentsOfDirectoryAtPath( path ) {
             for file in files {
+                //profiles.add(ProvisioningProfile(path: path + "/" + file))
                 profiles.append(ProvisioningProfile(path: path + "/" + file))
             }
         }
@@ -42,22 +43,64 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
 
         switch tableColumn!.identifier {
+        case "teamName":
+            return profiles[row].teamName
         case "name":
             return profiles[row].name
         case "expirationDate":
             return profiles[row].expirationDate
         case "createDate":
-            return profiles[row].createDate
+            return profiles[row].creationDate
         case "uuid":
             return profiles[row].uuid
         default:
-            return profiles[row].createDate
+            return "ERROR"
         }
     }
 
     func tableViewSelectionDidChange(notification: NSNotification) {
         webView.mainFrame.loadHTMLString(profiles[tableView.selectedRow].generateHTML(),baseURL: nil)
     }
+
+    func tableView(tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
+
+        for sortDescriptor in tableView.sortDescriptors {
+            var key = sortDescriptor.key as String!
+            switch key {
+            case "name":
+                if sortDescriptor.ascending {
+
+                    profiles.sortInPlace { (a,b) in return a.name < b.name }
+                } else {
+                    profiles.sortInPlace { (a,b) in return a.name > b.name }
+                }
+            case "teamName":
+                if sortDescriptor.ascending {
+                    profiles.sortInPlace { (a,b) in return a.teamName < b.teamName }
+                } else {
+                    profiles.sortInPlace { (a,b) in return a.teamName > b.teamName }
+                }
+            case "uuid":
+                if sortDescriptor.ascending {
+                    profiles.sortInPlace { (a,b) in return a.uuid < b.uuid }
+                } else {
+                    profiles.sortInPlace { (a,b) in return a.uuid > b.uuid }
+                }
+            default:
+                if sortDescriptor.ascending {
+                    profiles.sortInPlace { (a,b) in return a.uuid < b.uuid }
+                } else {
+                    profiles.sortInPlace { (a,b) in return a.uuid > b.uuid }
+                }
+            }
+            break; // 一回でいい
+        }
+        tableView.reloadData()
+    }
+
+    
+
+
 
 
 
